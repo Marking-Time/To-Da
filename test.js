fe78fe60e643c569d5262021ebfc7ae89c115142
Mark  King <g34519838@gmail.com>
2024-12-19 13:22:31 -0800

    testing createElement and appendChild

--- test.js
+++ test.js
@@ -1,11 +1,26 @@
 function display_data() {
     const displayRow = document.createElement("div");
 
-    const displayParagraph = document.createElement("p");
-    const pText = document.createTextNode("this is a paragraph inserted");
-    displayParagraph.appendChild(pText);
+    const displayDate = document.createElement("p");
+    displayDate.setAttribute("style","display:inline");
+    const date = document.createTextNode("Mon Dec 16 2024");
+    displayDate.appendChild(date);
 
-    displayRow.appendChild(displayParagraph);
+    const displayCustomer = document.createElement("p");
+    displayCustomer.setAttribute("style","display:inline");
+    const customer = document.createTextNode("Room 248");
+    displayCustomer.appendChild(customer);
+
+    const displayDescription = document.createElement("p");
+    displayDescription.setAttribute("style", "display:inline");
+    const description   =   document.createTextNode("This is a description");
+    displayDescription.appendChild(description);
+
+
+    displayRow.appendChild(displayDate);
+    displayRow.appendChild(displayCustomer);
+    displayRow.appendChild(displayDescription);
+
     const currentDiv = document.getElementById("display");
     document.body.insertBefore(displayRow,currentDiv);
 }
