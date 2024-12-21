63ab019e7427169d29f285ccf3ee47a635730a6e
Mark  King <g34519838@gmail.com>
2024-12-21 13:10:52 -0800

    Work on grid layout

--- test.js
+++ test.js
@@ -1,28 +1,29 @@
 function display_data() {
     const displayRow = document.createElement("div");
+    displayRow.setAttribute("class", "grid-container");
 
     const displayDate = document.createElement("p");
-    displayDate.setAttribute("class","grid-container");
+    // displayDate.setAttribute("class","grid-container");
     const date = document.createTextNode("Mon Dec 16 2024");
     displayDate.appendChild(date);
 
     const displayCustomer = document.createElement("p");
-    displayCustomer.setAttribute("class","grid-container");
+    // displayCustomer.setAttribute("class","grid-container");
     const customer = document.createTextNode("Room 248");
     displayCustomer.appendChild(customer);
 
     const displayDescription = document.createElement("p");
-    displayDescription.setAttribute("class", "grid-container");
+    // displayDescription.setAttribute("class", "grid-container");
     const description = document.createTextNode("This is a description");
     displayDescription.appendChild(description);
 
     const displayTechnician = document.createElement("p");
-    displayTechnician.setAttribute("class", "grid-container");
+    // displayTechnician.setAttribute("class", "grid-container");
     const technician = document.createTextNode("Unassigned");
     displayTechnician.appendChild(technician);
 
     const displayStatus = document.createElement("p");
-    displayStatus.setAttribute("class", "grid-container");
+    // displayStatus.setAttribute("class", "grid-container");
     const status = document.createTextNode("Open");
     displayStatus.appendChild(status);
 
