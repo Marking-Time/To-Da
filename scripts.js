56838d6c48d205a1dcc4544cbbee6e369afe1924
Mark  King <g34519838@gmail.com>
2024-12-30 09:01:03 -0800

    style form - copy saveData() into scripts.js

--- scripts.js
+++ scripts.js
@@ -61,4 +61,41 @@ function save_order() {
 
 }
 
+// displays data from local storage
+function display_data() {
+    const displayRow = document.createElement("div");
+    displayRow.setAttribute("class", "grid-container");
+
+    const displayDate = document.createElement("p");
+    const date = document.createTextNode("Mon Dec 16 2024");
+    displayDate.appendChild(date);
+
+    const displayCustomer = document.createElement("p");
+    const customer = document.createTextNode("Room 248");
+    displayCustomer.appendChild(customer);
+
+    const displayDescription = document.createElement("p");
+    const description = document.createTextNode("This is a description");
+    displayDescription.appendChild(description);
+
+    const displayTechnician = document.createElement("p");
+    const technician = document.createTextNode("Unassigned");
+    displayTechnician.appendChild(technician);
+
+    const displayStatus = document.createElement("p");
+    const status = document.createTextNode("Open");
+    displayStatus.appendChild(status);
+
+    displayRow.appendChild(displayDate);
+    displayRow.appendChild(displayCustomer);
+    displayRow.appendChild(displayDescription);
+    displayRow.appendChild(displayTechnician);
+    displayRow.appendChild(displayStatus);
+
+    const parentDiv = document.getElementById("display").parentNode;
+
+    const currentDiv = document.getElementById("display");
+    parentDiv.insertBefore(displayRow,currentDiv);
+}
+
 
