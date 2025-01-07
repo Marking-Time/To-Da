fcd41ced11ddef4331dc2b635c1e081f27b60479
Mark  King <g34519838@gmail.com>
2025-01-07 15:11:49 -0800

    CRUD-Read complete

--- scripts.js
+++ scripts.js
@@ -56,14 +56,39 @@ function save_order() {
     
     localStorage.setItem("ordersList",order2save);
 
+    set_variables();
+
  }
 
 //  
-// gets data from local storage and returns an array of order numbers
+// gets data from local storage and returns an array of order numbers and displays on the index
+
 function get_LS_ordersList() {
     getOrders = localStorage.getItem("ordersList");
     getOrders = getOrders.split(','); 
     return getOrders;
+}
+
+function set_variables() {
+    getOrders = get_LS_ordersList();
+
+    for (let i = 0; i < getOrders.length; i++) {
+        // console.log(getOrders[i]);
+        if (i != "0") {
+            orderRow = localStorage.getItem(getOrders[i]);
+            orderRow = orderRow.split(',');
+            console.log(orderRow);
+            rowDate = orderRow[0];
+            rowCustomer = orderRow[1];
+            rowDescription = orderRow[2];
+            rowAssigned = orderRow[3];
+            rowStatus = orderRow[4];
+
+            display_data();
+            
+        }
+    }
+
 }
 
 function display_data() {
@@ -72,23 +97,23 @@ function display_data() {
     displayRow.setAttribute("class", "grid-container");
 
     const displayDate = document.createElement("p");
-    const date = document.createTextNode("Mon Dec 16 2024");
+    const date = document.createTextNode(rowDate);
     displayDate.appendChild(date);
 
     const displayCustomer = document.createElement("p");
-    const customer = document.createTextNode("Room 248");
+    const customer = document.createTextNode(rowCustomer);
     displayCustomer.appendChild(customer);
 
     const displayDescription = document.createElement("p");
-    const description = document.createTextNode("This is a description");
+    const description = document.createTextNode(rowDescription);
     displayDescription.appendChild(description);
 
     const displayTechnician = document.createElement("p");
-    const technician = document.createTextNode("Unassigned");
+    const technician = document.createTextNode(rowAssigned);
     displayTechnician.appendChild(technician);
 
     const displayStatus = document.createElement("p");
-    const status = document.createTextNode("Open");
+    const status = document.createTextNode(rowStatus);
     displayStatus.appendChild(status);
 
     displayRow.appendChild(displayDate);
@@ -102,3 +127,6 @@ function display_data() {
     const currentDiv = document.getElementById("display");
     parentDiv.insertBefore(displayRow,currentDiv);
 }
+
+//  Run
+set_variables();
