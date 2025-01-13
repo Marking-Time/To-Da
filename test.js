3a6d68944644cdea321a23ec308d0e5dc43fb693
Mark  King <g34519838@gmail.com>
2025-01-13 07:57:57 -0800

    replace code in test.js with production proior to implementing update/delete

--- test.js
+++ test.js
@@ -1,4 +1,68 @@
-// gets data from local storage and returns an array of order numbers
+//  CRUD functions:
+//  CRUD -Create
+
+// Check to see if data in localstorage
+function is_Initiated() {
+    if (localStorage.key('initiated')) {
+        increment = localStorage.getItem('increment');
+        increment = Number(increment);
+        console.log(increment);
+        // return increment;
+    }
+}
+
+//  Returns a string of the current date and time in local time zone
+function date_now() {
+    let d1 = new Date();
+    d1.toUTCString();
+    let d2 = d1.toString();
+    d2 = d2.slice(0,24);
+    return d2
+}
+
+// The follwing add to ordersList and remove from ordersList
+// They need to called from within the save_order() function
+function add_order() {
+    let orderNumber = localStorage.getItem('increment');
+    orderNumber = Number(orderNumber);
+    orderNumber +=1;
+
+    let orders =  localStorage.getItem('ordersList');
+    orders = orders.split(',');
+    orders = orders.push(orderNumber);
+
+
+    return orders;
+}
+
+function save_order() {
+
+    date = date_now();
+    let orderNumber = localStorage.getItem('increment');
+    orderNumber = Number(orderNumber);
+    orderNumber +=1;
+    
+    const customer = document.getElementById("customer");
+    const description = document.getElementById("description");
+    const tech = document.getElementById("tech");
+    const status = document.getElementById("status");
+    const order = [date, customer.value, description.value, tech.value, status.value];
+    localStorage.setItem(orderNumber, order);
+    localStorage.setItem('increment',orderNumber);
+
+    let order2save = [];
+    order2save.push(localStorage.getItem('ordersList'));
+    order2save.push(orderNumber);
+    
+    localStorage.setItem("ordersList",order2save);
+
+    set_variables();
+
+ }
+
+//  
+// gets data from local storage and returns an array of order numbers and displays on the index
+
 function get_LS_ordersList() {
     getOrders = localStorage.getItem("ordersList");
     getOrders = getOrders.split(','); 
@@ -63,3 +127,6 @@ function display_data() {
     const currentDiv = document.getElementById("display");
     parentDiv.insertBefore(displayRow,currentDiv);
 }
+
+//  Run
+set_variables();
