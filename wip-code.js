a3cc5dba82c6cb8aa1883642a3f41bc0635c0b7c
Mark  King <g34519838@gmail.com>
2025-02-13 09:14:46 -0800

    create work in progress

---
+++ wip-code.js
@@ -0,0 +1,13 @@
+function compare(){ 
+if (compareDescription == document.getElementById("compareDescription").value)
+{
+return true
+}}
+
+result = false
+function compare() {
+	if (updateDescription != document.getElementById("compareDescription").value
+	&& document.getElementById("compareDescription").value != "")  {
+	result = true;
+	}
+return result
